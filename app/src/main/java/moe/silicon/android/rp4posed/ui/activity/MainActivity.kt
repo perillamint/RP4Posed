0f079f4b8f267bea744a742368308081300d4a2e
perillamint <perillamint@silicon.moe>
2023-05-02 02:04:41 +0900

    Remove app icon hide menu

--- app/src/main/java/moe/silicon/android/rp4posed/ui/activity/MainActivity.kt
+++ app/src/main/java/moe/silicon/android/rp4posed/ui/activity/MainActivity.kt
@@ -3,7 +3,10 @@
 package moe.silicon.android.rp4posed.ui.activity
 
 import android.content.ComponentName
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
+import android.util.Log
 import androidx.core.view.isVisible
 import com.highcapable.yukihookapi.YukiHookAPI
 import moe.silicon.android.rp4posed.BuildConfig
@@ -11,47 +14,24 @@ import moe.silicon.android.rp4posed.R
 import moe.silicon.android.rp4posed.databinding.ActivityMainBinding
 import moe.silicon.android.rp4posed.ui.activity.base.BaseActivity
 
+const val TAG = "RP4PosedMainActivity"
+
 class MainActivity : BaseActivity<ActivityMainBinding>() {
 
     override fun onCreate() {
         refreshModuleStatus()
         binding.mainTextVersion.text = getString(R.string.module_version, BuildConfig.VERSION_NAME)
-        binding.hideIconInLauncherSwitch.isChecked = isLauncherIconShowing.not()
-        binding.hideIconInLauncherSwitch.setOnCheckedChangeListener { button, isChecked ->
-            if (button.isPressed) hideOrShowLauncherIcon(isChecked)
+
+        binding.mainLinStatus.setOnClickListener {
+            Log.d(TAG, "Broadcasting LSPosed magic secret code!")
+            var intent = Intent("android.provider.Telephony.SECRET_CODE", Uri.parse("android_secret_code://5776733"))
+            sendBroadcast(intent)
         }
+
+        binding.hijackButton.isChecked = true
+        binding.hijackButton.isEnabled = false
         // Your code here.
     }
-
-    /**
-     * Hide or show launcher icons
-     *
-     * - You may need the latest version of LSPosed to enable the function of hiding launcher
-     *   icons in higher version systems
-     *
-     * 隐藏或显示启动器图标
-     *
-     * - 你可能需要 LSPosed 的最新版本以开启高版本系统中隐藏 APP 桌面图标功能
-     * @param isShow Whether to display / 是否显示
-     */
-    private fun hideOrShowLauncherIcon(isShow: Boolean) {
-        packageManager?.setComponentEnabledSetting(
-            ComponentName(packageName, "${BuildConfig.APPLICATION_ID}.Home"),
-            if (isShow) PackageManager.COMPONENT_ENABLED_STATE_DISABLED else PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
-            PackageManager.DONT_KILL_APP
-        )
-    }
-
-    /**
-     * Get launcher icon state
-     *
-     * 获取启动器图标状态
-     * @return [Boolean] Whether to display / 是否显示
-     */
-    private val isLauncherIconShowing
-        get() = packageManager?.getComponentEnabledSetting(
-            ComponentName(packageName, "${BuildConfig.APPLICATION_ID}.Home")
-        ) != PackageManager.COMPONENT_ENABLED_STATE_DISABLED
 
     /**
      * Refresh module status
